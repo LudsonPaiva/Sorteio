b5899e6f9b50f8de7fd97a5f5d76e64a9886a1b9
LudsonPaiva <ludsonpaiva@yahoo.com.br>
2022-11-03 19:01:02 -0300

    Projeto final

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+
+# Criar uma lista com números entre 1 e ...
+lista = list(range(1,58))
+
+# Embaralha a lista de números
+random.shuffle(lista)
--- sorteio_master.py
+++ sorteio_master.py
@@ -1,14 +1,40 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from openpyxl import Workbook, load_workbook
-from openpyxl.utils import get_column_letter
+from openpyxl import load_workbook
+
+from main import lista
 
 arquivo_excel_1 = 'base_sorteio.xlsx'
 
 df_nomes = pd.read_excel(arquivo_excel_1, sheet_name='Funcionário')
 df_produtos = pd.read_excel(arquivo_excel_1, sheet_name='Produtos')
-# df_nomes.at[all(), "A"] = df_produtos.at[all(), "F"]
+
+# Criando a o dataframe com os números sorteados
+df_lista = pd.DataFrame({'Ordem':lista})
+
+# Criando a página ordem e incluindo a Lista
+book = load_workbook(arquivo_excel_1)
+writer = pd.ExcelWriter(arquivo_excel_1, engine='openpyxl')
+writer.book = book
+df_ordem = df_lista.copy()
+df_ordem.to_excel(writer, sheet_name='Ordem', index=False)
+
+writer.save()
+writer.close()
+
+# realizando o sorteio
+df_sorteio = pd.concat([df_ordem, df_nomes], axis = 1)
+df_resultado = pd.merge(df_sorteio, df_produtos, on='Ordem')
+
+# salvando o sorteio na lista
+book = load_workbook(arquivo_excel_1)
+writer = pd.ExcelWriter(arquivo_excel_1, engine='openpyxl')
+writer.book = book
+df_ordem = df_resultado.copy()
+df_ordem.to_excel(writer, sheet_name='Sorteio', index=False)
+
+writer.save()
+writer.close()
+
+print(df_resultado)
 
 
-print(df_nomes, df_produtos)
